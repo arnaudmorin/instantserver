16eb765a9ae2b166920d12c234dd465f5dd12100
Arnaud Morin <arnaud1.morin@orange.com>
2015-02-12 11:43:40 +0100

    Add create vm test script

---
+++ create-vm.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python
+import os
+import pprint
+import novaclient.v1_1.client as nvclient
+pp = pprint.PrettyPrinter(indent=4)
+
+def get_keystone_creds():
+    d = {}
+    d['username'] = os.environ['OS_USERNAME']
+    d['password'] = os.environ['OS_PASSWORD']
+    d['auth_url'] = os.environ['OS_AUTH_URL']
+    d['tenant_name'] = os.environ['OS_TENANT_NAME']
+    return d
+    
+def get_nova_creds():
+    d = {}
+    d['username'] = os.environ['OS_USERNAME']
+    d['api_key'] = os.environ['OS_PASSWORD']
+    d['auth_url'] = os.environ['OS_AUTH_URL']
+    d['project_id'] = os.environ['OS_TENANT_NAME']
+    return d
+
+def get_server(creds, servername):
+    nova = nvclient.Client(**creds)
+    return nova.servers.find(name=servername)
+
+def remove_hostkey(ip):
+    subprocess.call(["ssh-keygen", "-R", ip])
+
+def get_hostkey_from_console(text):
+    lines = text.split('\n')
+    start = '-----BEGIN SSH HOST KEY KEYS-----\r'
+    end = '-----END SSH HOST KEY KEYS-----\r'
+    start_ind = lines.index(start)
+    end_ind = lines.index(end)
+    for i in range(start_ind+1, end_ind):
+        key = lines[i].rstrip()
+        if key.startswith('ssh-rsa'):
+            return key
+    raise KeyError("ssh host key not found")
+
+def print_title(title):
+    print "\n"+"#"*32+"\n# "+title+"\n"+"#"*32+"\n"
+    
+def main():
+    creds = get_nova_creds()
+    nova = nvclient.Client(**creds)
+
+    print_title("servers list")
+    #servers = nova.servers.list()
+    #pp.pprint(servers)
+
+    sprout = nova.servers.find(name='Sprout')
+    #pp.pprint(sprout.__dict__)
+
+    f = nova.flavors.find(name = 'm1.tiny')
+    i = nova.images.find(name = 'Ubuntu_14.04_LTS')
+    n = nova.networks.find(label = 's_Cld4Net_internal_net')
+    #pp.pprint(f.__dict__)
+
+    nova.servers.create(
+        name = "my-server", 
+        flavor = f,
+        image = i,
+        nics = [{"net-id": n.id}],
+    )
+
+
+
+if __name__ == '__main__':
+    main()
--- views.py
+++ views.py
@@ -7,43 +7,40 @@ import random
 import string
 
 def index(request):
-	"""Index page"""
-	context = {}
-	return render(request, 'instantserver/index.html', context)
+    """Index page"""
+    context = {}
+    return render(request, 'instantserver/index.html', context)
 
 def vm_list(request):
-	"""List all VM"""
-	if request.method == 'POST':
-		virtual_machine = VirtualMachine()
-		
-		# Temporary hack to return a valid machine
-		# TODO: make request over opensteak API
-		virtual_machine.ip_address = "192.168.1.{}".format(random.randrange(2, 254))
-		virtual_machine.creation_date = timezone.now()
-		virtual_machine.login = "ubuntu"
-		virtual_machine.password = ''.join([random.choice(string.ascii_letters + string.digits) for n in range(10)])
-		
-		# Create json object from VM
-		json_object = {	'ip_address': virtual_machine.ip_address,
-						'creation_date': str(virtual_machine.creation_date),
-						'login': virtual_machine.login,
-						'password': virtual_machine.password
-					  }
-		return HttpResponse(json.dumps(json_object), content_type="application/json")
-	else:
-		virtual_machines = VirtualMachine.objects.all()
-		context = {'virtual_machines': virtual_machines}
-		return render(request, 'instantserver/vm_list.html', context)
-	
+    """List all VM"""
+    if request.method == 'POST':
+        virtual_machine = VirtualMachine()
+        virtual_machine.ip_address = "192.168.1.{}".format(random.randrange(2, 254))
+        virtual_machine.creation_date = timezone.now()
+        virtual_machine.login = "ubuntu"
+        virtual_machine.password = ''.join([random.choice(string.ascii_letters + string.digits) for n in range(10)])
+        
+        # Create json object from VM
+        json_object = { 'ip_address': virtual_machine.ip_address,
+                        'creation_date': str(virtual_machine.creation_date),
+                        'login': virtual_machine.login,
+                        'password': virtual_machine.password
+                      }
+        return HttpResponse(json.dumps(json_object), content_type="application/json")
+    else:
+        virtual_machines = VirtualMachine.objects.all()
+        context = {'virtual_machines': virtual_machines}
+        return render(request, 'instantserver/vm_list.html', context)
+    
 
 def vm_id(request, vm_id):
-	"""Detail of one VM"""
-	virtual_machine = get_object_or_404(VirtualMachine, pk=vm_id)
-	context = {'vm': virtual_machine}
-	return render(request, 'instantserver/vm.html', context)
+    """Detail of one VM"""
+    virtual_machine = get_object_or_404(VirtualMachine, pk=vm_id)
+    context = {'vm': virtual_machine}
+    return render(request, 'instantserver/vm.html', context)
 
 def vm_ip(request, vm_ip):
-	"""Detail of one VM"""
-	virtual_machine = get_object_or_404(VirtualMachine, ip_address=vm_ip)
-	context = {'vm': virtual_machine}
-	return render(request, 'instantserver/vm.html', context)
+    """Detail of one VM"""
+    virtual_machine = get_object_or_404(VirtualMachine, ip_address=vm_ip)
+    context = {'vm': virtual_machine}
+    return render(request, 'instantserver/vm.html', context)
