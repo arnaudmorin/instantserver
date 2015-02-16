80815e7b3dde800953aa5aa2ca2ab8f297eb0343
arnaud <arnaud@ptikiki>
2015-02-16 11:51:14 +0100

    Update create-vm to use opensteak things and security group

--- create-vm.py
+++ create-vm.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python
 import os
 import pprint
-import novaclient.v1_1.client as nvclient
+import novaclient.v1_1.client as novaclient
 pp = pprint.PrettyPrinter(indent=4)
 
-def get_keystone_creds():
-    d = {}
-    d['username'] = os.environ['OS_USERNAME']
-    d['password'] = os.environ['OS_PASSWORD']
-    d['auth_url'] = os.environ['OS_AUTH_URL']
-    d['tenant_name'] = os.environ['OS_TENANT_NAME']
-    return d
-    
-def get_nova_creds():
+def p(value):
+    """Shortcut for pretty printing"""
+    pp.pprint(value)
+
+def print_title(title):
+    """Print title of things"""
+    print "\n"+"#"*32+"\n# "+title+"\n"+"#"*32+"\n"
+
+def get_creds():
+    """Retrieve creds from environment"""
     d = {}
     d['username'] = os.environ['OS_USERNAME']
     d['api_key'] = os.environ['OS_PASSWORD']
@@ -20,52 +21,38 @@ def get_nova_creds():
     d['project_id'] = os.environ['OS_TENANT_NAME']
     return d
 
-def get_server(creds, servername):
-    nova = nvclient.Client(**creds)
-    return nova.servers.find(name=servername)
-
-def remove_hostkey(ip):
-    subprocess.call(["ssh-keygen", "-R", ip])
-
-def get_hostkey_from_console(text):
-    lines = text.split('\n')
-    start = '-----BEGIN SSH HOST KEY KEYS-----\r'
-    end = '-----END SSH HOST KEY KEYS-----\r'
-    start_ind = lines.index(start)
-    end_ind = lines.index(end)
-    for i in range(start_ind+1, end_ind):
-        key = lines[i].rstrip()
-        if key.startswith('ssh-rsa'):
-            return key
-    raise KeyError("ssh host key not found")
-
-def print_title(title):
-    print "\n"+"#"*32+"\n# "+title+"\n"+"#"*32+"\n"
-    
 def main():
-    creds = get_nova_creds()
-    nova = nvclient.Client(**creds)
+    creds = get_creds()
+    nova = novaclient.Client(**creds)
 
-    print_title("servers list")
+    #print_title("servers list")
     #servers = nova.servers.list()
-    #pp.pprint(servers)
+    #p(servers)
 
-    sprout = nova.servers.find(name='Sprout')
-    #pp.pprint(sprout.__dict__)
+    #sprout = nova.servers.find(name='Sprout')
+    #p(sprout.__dict__)
 
-    f = nova.flavors.find(name = 'm1.tiny')
-    i = nova.images.find(name = 'Ubuntu_14.04_LTS')
-    n = nova.networks.find(label = 's_Cld4Net_internal_net')
-    #pp.pprint(f.__dict__)
+    f = nova.flavors.find(name = 'instantserver-small')
+    i = nova.images.find(name = 'Ubuntu 14.10 64b')
+    n = nova.networks.find(label = 'instantserver')
+    k = "idrsa-sansmotdepasse"
+    u = "#cloud-config\npassword: moutarde\nchpasswd: { expire: False }\nssh_pwauth: True"
+    #s = nova.security_groups.find(name="Tout-Autorise-Entrant-Sortant")
+    #p(s.__dict__)
 
-    nova.servers.create(
-        name = "tutu-server", 
+    new_server = nova.servers.create(
+        name = "instantserver-1", 
         flavor = f,
         image = i,
         nics = [{"net-id": n.id}],
-        key_name = "id-rsa-sans-mot-de-passe"
+        key_name = k,
+        userdata = u,
+        security_groups = ["Tout-Autorise-Entrant-Sortant"]
     )
 
+    # Add security group just after creation of the machine so that it will be able to contact
+    # the metadata services
+    #new_server.add_security_group(s)
 
 
 if __name__ == '__main__':
