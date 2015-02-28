83bc20ada452538bcf23b97a25b8b61cc6cab2b4
arnaud <arnaud@ptikiki>
2015-02-28 11:23:25 +0100

    Merge opensteak & openwatt

--- create-vm.py
+++ create-vm.py
@@ -2,6 +2,7 @@
 import os
 import pprint
 import novaclient.v1_1.client as novaclient
+import cinderclient.v1.client as cinderclient
 pp = pprint.PrettyPrinter(indent=4)
 
 def p(value):
@@ -12,40 +13,102 @@ def print_title(title):
     """Print title of things"""
     print "\n"+"#"*32+"\n# "+title+"\n"+"#"*32+"\n"
 
-def get_creds():
-    """Retrieve creds from environment"""
-    d = {}
-    d['username'] = os.environ['OS_USERNAME']
-    d['api_key'] = os.environ['OS_PASSWORD']
-    d['auth_url'] = os.environ['OS_AUTH_URL']
-    d['project_id'] = os.environ['OS_TENANT_NAME']
-    return d
+def get_credentials(service):
+    """Returns a creds dictionary filled with the following keys:
+
+    * username
+    * password/api_key (depending on the service)
+    * tenant_name/project_id (depending on the service)
+    * auth_url
+
+    :param service: a string indicating the name of the service
+                    requesting the credentials.
+    """
+
+    creds = {}
+    # Unfortunately, each of the openstack client will request slightly
+    # different entries in their credentials dict.
+    if service.lower() in ("nova", "cinder"):
+        password = "api_key"
+        tenant = "project_id"
+    else:
+        password = "password"
+        tenant = "tenant_name"
+
+    # The most common way to pass these info to your script is to do it through
+    # your environment variables. Since we're `get`ing values from a
+    # dict, it shouldn't be too difficult to set default values too.
+    creds.update({
+        "username": os.environ.get('OS_USERNAME', "demo"),
+        password: os.environ.get("OS_PASSWORD", "password"),
+        "auth_url": os.environ.get("OS_AUTH_URL",
+                                   "http://192.168.0.10:5000/v2.0"),
+        tenant: os.environ.get("OS_TENANT_NAME", "invisible_to_admin"),
+    })
+
+    return creds
+
+def create_volume(c,i):
+    """Create a volume from image"""
+    return c.volumes.create(
+        size = "10",
+        display_name = "instantserver-1",
+        display_description = "Volume for instantserver-1",
+        imageRef = i
+    )
 
 def main():
-    creds = get_creds()
+    creds = get_credentials("nova")
     nova = novaclient.Client(**creds)
+    cinder = cinderclient.Client(**creds)
 
     print_title("servers list")
-    pp.pprint(nova.servers.list())
+    p(nova.servers.list())
 
     print_title("images list")
-    pp.pprint(nova.images.list())
+    p(nova.images.list())
 
-    #sprout = nova.servers.find(name='Sprout')
-    #pp.pprint(sprout.__dict__)
+    # Create a volume
 
+    new_server = nova.servers.create(
+        name = "instantserver-1", 
+        flavor = f,
+        image = i,
+        nics = [{"net-id": n.id}],
+        key_name = k,
+        userdata = u,
+        security_groups = s
+    )
+
+    # OpenWatt
     f = nova.flavors.find(name = 'm1.tiny')
     i = nova.images.find(name = 'Ubuntu 14.04.1 LTS')
     n = nova.networks.find(label = 's_Cld4Net_internal_net')
-    #pp.pprint(f.__dict__)
+    u = "#cloud-config\npassword: moutarde\nchpasswd: { expire: False }\nssh_pwauth: True"
+    k = "idrsa-sansmotdepasse"
+    s = ["default"]
 
-    #nova.servers.create(
-    #    name = "tutu-server", 
-    #    flavor = f,
-    #    image = i,
-    #    nics = [{"net-id": n.id}],
-    #    key_name = "id-rsa-sans-mot-de-passe"
-    #)
+    # OpenSteak
+    #f = nova.flavors.find(name = 'instantserver-small')
+    #i = nova.images.find(name = 'Ubuntu 14.10 64b')
+    #n = nova.networks.find(label = 'instantserver')
+    #u = "#cloud-config\npassword: moutarde\nchpasswd: { expire: False }\nssh_pwauth: True"
+    #k = "idrsa-sansmotdepasse"
+    #s = ["Tout-Autorise-Entrant-Sortant"]
+    #v = create_volume(cinder, i.id)
+
+    # Do create
+    new_server = nova.servers.create(
+        name               = "instantserver-1", 
+        flavor             = f,
+        image              = i,
+        nics               = [{"net-id": n.id}],
+        key_name           = k,
+        userdata           = u,
+        security_groups    = s
+    )
+
+    p(new_server)
 
 if __name__ == '__main__':
     main()
